43e72f98d8edca91b9fc17ef09ca66cc86082fa7
Aung Myint <your-real-github-email@example.com>
2025-08-26 19:40:17 +0630

    fox main

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,20 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
+import App from './App.tsx';
+import DebugEnv from './pages/DebugEnv.tsx';  // 👈 import your new page
+import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <Router>
+      <Routes>
+        {/* your main app */}
+        <Route path="/*" element={<App />} />
+        {/* debug page */}
+        <Route path="/debug-env" element={<DebugEnv />} />
+      </Routes>
+    </Router>
+  </React.StrictMode>
+);
